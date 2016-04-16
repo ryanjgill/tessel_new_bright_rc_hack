951f717c3bcd7b24cbc0aaa38e9df28667bd74eb
ryanjgill <ryanjgill2@gmail.com>
2016-04-16 17:35:32 -0500

    stubbing out functions and websockets

---
+++ app.js
@@ -0,0 +1,163 @@
+'use strict'
+
+// node express
+let express = require('express')
+let path = require('path')
+let favicon = require('serve-favicon')
+let logger = require('morgan')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+
+let app = express()
+
+let httpServer = require('http').Server(app)
+let io = require('socket.io')(httpServer)
+
+let tessel = require('tessel')
+let pin0 = tessel.port.A.pin[0]
+let pin1 = tessel.port.A.pin[1]
+let pin2 = tessel.port.A.pin[2]
+let pin3 = tessel.port.A.pin[3]
+
+pin0.output(0)
+pin1.output(0)
+pin2.output(0)
+pin3.output(0)
+
+function forward(p1, p2) {
+  console.log('forward!')
+  p1.output(1)
+  p2.output(0)
+}
+
+function reverse(p1, p2) {
+  console.log('reverse!')
+  p1.output(0)
+  p2.output(1)
+}
+
+function brake(p1, p2) {
+  console.log('brake!')
+  p1.output(0)
+  p2.output(0)
+}
+
+function steerLeft(p1, p2) {
+  steerStraight(pin2, pin3)
+  p1.output(1)
+  p2.output(0)
+  forward(pin0, pin1)
+}
+
+function steerRight(p1, p2) {
+  steerStraight(pin2, pin3)
+  p1.output(0)
+  p2.output(1)
+  forward(pin0, pin1)
+}
+
+function steerStraight(p1, p2) {
+  p1.output(0)
+  p2.output(0)
+  brake(pin0, pin1)
+}
+
+
+// defining pin variables
+var forwardPin, reversePin, leftPin, rightPin
+
+httpServer.listen(3000)
+
+io.on('connection', function (socket) {
+  console.log(`New connection to socketId: ${socket.id}`)
+
+  // emit usersCount on new connection
+  emitUsersCount(io)
+
+  // emit usersCount when connection is closed
+  socket.on('disconnect', function () {
+    emitUsersCount(io)
+  })
+
+  socket.on('command:forward:on', function (data) {
+    forward(pin0, pin1)
+    console.log('command received! --> FORWARD ON')
+  })
+
+  // Steering Commands
+  socket.on('command:forward:off', function (data) {
+    brake(pin0, pin1)
+    console.log('command received! --> FORWARD OFF')
+  })
+
+  socket.on('command:reverse:on', function (data) {
+    reverse(pin0, pin1)
+    console.log('command received! --> REVERSE ON')
+  })
+
+  socket.on('command:reverse:off', function (data) {
+    brake(pin0, pin1)
+    console.log('command received! --> REVERSE OFF')
+  })
+
+  socket.on('command:left:on', function (data) {
+    steerLeft(pin2, pin3)
+    console.log('command received! --> LEFT ON')
+  })
+
+  socket.on('command:left:off', function (data) {
+    steerStraight(pin2, pin3)
+    console.log('command received! --> LEFT OFF')
+  })
+
+  socket.on('command:right:on', function (data) {
+    steerRight(pin2, pin3)
+    console.log('command received! --> RIGHT ON')
+  })
+
+  socket.on('command:right:off', function (data) {
+    steerStraight(pin2, pin3)
+    console.log('command received! --> RIGHT OFF')
+  })
+})
+
+
+
+// emit usersCount to all sockets
+function emitUsersCount(io) {
+  io.sockets.emit('usersCount', {
+    totalUsers: io.engine.clientsCount
+  })
+}
+
+// emit signal received to all sockets
+function emitSignalReceived(io, message) {
+  io.sockets.emit('signal:received', {
+    date: new Date().getTime(),
+    value: message || 'Signal received.'
+  })
+}
+
+
+// setting app stuff
+app.locals.title = 'Tessel 2 New Bright RC Hack'
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+//app.set('view engine', 'jade')
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Routes
+app.get('/', function(req, res, next) {
+  res.send('/public/index.html')
+})
+
+console.log("Server running at http://192.168.1.101:3000/")
---
+++ public/javascripts/carControls.js
@@ -0,0 +1,43 @@
+$(function () {
+    var socket = io()
+
+    socket.on('led:change', function (data) {
+        console.log(data)
+    })
+
+    socket.on('signal:received', function (data) {
+        console.log(data)
+    })
+
+    $('#forward').mousedown(function () {
+        socket.emit('command:forward:on')
+    })
+
+    $('#forward').mouseup(function () {
+        socket.emit('command:forward:off')
+    })
+
+    $('#reverse').mousedown(function () {
+        socket.emit('command:reverse:on')
+    })
+
+    $('#reverse').mouseup(function () {
+        socket.emit('command:reverse:off')
+    })
+
+    $('#left').mousedown(function () {
+        socket.emit('command:left:on')
+    })
+
+    $('#left').mouseup(function () {
+        socket.emit('command:left:off')
+    })
+
+    $('#right').mousedown(function () {
+        socket.emit('command:right:on')
+    })
+
+    $('#right').mouseup(function () {
+        socket.emit('command:right:off')
+    })
+})
